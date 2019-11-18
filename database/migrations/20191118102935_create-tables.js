35dd6f1eebea18ab7727c7d9b8e906c9315afb48
Ian Schwartz <ian.schwartz23@gmail.com>
2019-11-18 11:09:05 -0800

    create tables migration

---
+++ database/migrations/20191118102935_create-tables.js
@@ -0,0 +1,65 @@
+exports.up = function(knex) {
+  return knex.schema
+
+  .createTable('users', users => {
+    users.increments();
+    
+    users
+    .string('email', 128)
+    .notNullable()
+
+    users.string('name', 128).notNullable();
+    users.string('password', 128).notNullable();
+  })
+  
+  .createTable('tourists', tourists => {
+    tourists.increments();
+
+    tourists 
+      .string('firstname', 128)
+      .notNullable()
+    tourists.string('lastname', 128).notNullable();
+    tourists.integer('user_id').unsigned().notNullable().references('id').inTable('users');
+  })
+
+  .createTable('guides', guides => {
+    guides.increments();
+
+    guides
+      .string('firstname', 128)
+      .notNullable()
+    guides.string('lastname', 128).notNullable();
+    guides.integer('user_id').unsigned().notNullable().references('id').inTable('users');
+  })
+
+  .createTable('trips', trips => {
+    trips.increments();
+
+    trips
+      .string('tourname', 128)
+      .notNullable()
+    
+    trips.string('description', 255).notNullable();
+    trips.string('price', 128).notNullable();
+    trips.string('duration', 128).notNullable();
+    trips.string('location', 128).notNullable();
+    trips.string('language', 128)
+    })
+
+    .createTable('users_trips', tbl => {
+        tbl.increments();
+        tbl.integer('user_id').unsigned().notNullable().references('id').inTable('users');
+        tbl.integer('tourist_id').unsigned().notNullable().references('id').inTable('tourists');
+        tbl.integer('guide_id').unsigned().notNullable().references('id').inTable('guides');
+        tbl.integer('trip_id').unsigned().notNullable().references('id').inTable('trips');
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('users_trips')
+    .dropTableIfExists('trips')
+    .dropTableIfExists('guides')
+    .dropTableIfExists('tourists')
+    .dropTableIfExists('users')
+};

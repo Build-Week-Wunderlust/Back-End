37d9b8bfbaefd9fd49b0a0fcd7c820e4adb81eba
Ian Schwartz <ian.schwartz23@gmail.com>
2019-11-19 10:45:48 -0800

    trips model and router

---
+++ trips/trips-model.js
@@ -0,0 +1,42 @@
+const db = require('../../database/dbConfig');
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById,
+  update,
+  remove
+};
+
+function find() {
+  return db('users').select('id', 'username');
+}
+
+function findBy(filter) {
+  return db('users').where(filter);
+}
+
+async function add(user) {
+  const [id] = await db('users').insert(user);
+
+  return findById(id);
+}
+
+function findById(id) {
+  return db('users')
+    .where({ id })
+    .first();
+}
+
+function update(changes, id) {
+    return db('trips')
+      .where({ id })
+      .update(changes);
+}
+
+function remove(id) {
+    return db('trips')
+      .where({ id })
+      .delete();
+}
---
+++ trips/trips-router.js
@@ -0,0 +1,50 @@
+const express = require('express');
+
+const Trips = require('./trips-model');
+
+const authenticate = require('../auth/restricted-middleware');
+
+const router = express.Router();
+
+router.get('/trips', (req, res) => {
+    Trips.find()
+      .then(trips => res.status(200).json(trips))
+      .catch(err => res.status(500).json({ error: err }))
+});
+
+router.get('/trips/:id', (req, res) => {
+    Trips.findById(req.params.id)
+      .then(trip => {
+          if (trip) {
+              res.status(200).json(trip)
+          } else {
+              res.status(400).json({ message: 'Trip doesnt exist' })
+          }
+      })
+      .catch(err => res.status(500).json({ error: err }))
+});
+
+router.post('/trips', authenticate, (req, res) => {
+    Trips.add(req.body)
+      .then(trips => res.status(200).json(trips))
+      .catch(err => res.status(500).json({ error: err }))
+});
+
+router.put('/trips/:id', authenticate, (req, res) => {
+    Trips.findById(req.params.id)
+      .then(trip => {
+          if (trip) {
+              Trips.update(req.body, req.params.id)
+                .then(updated => res.json(updated))
+          } else {
+              res.status(404).json({ message: 'Trip doesnt exist' })
+          }
+      })
+      .catch(err => res.status(500).json({ error: err }))
+});
+
+router.delete('/trips/:id', authenticate, (req, res) => {
+    Trips.remove(req.params.id)
+      .then(trip => res.json({ message: `${trip} has been deleted` }))
+      .catch(err => res.status(500).json({ error: err }))
+});

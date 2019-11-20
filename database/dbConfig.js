999a74d1791c08f6218985ba958993f3146a8545
Darren J Carrillo <darrenjcarrillo@gmail.com>
2019-11-20 12:15:07 -0800

    Debugging again

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,6 +2,6 @@ const knex = require("knex");
 
 const knexConfig = require("../knexfile");
 
-const environment = process.env.DB_ENV || "development";
+const environment = process.env.NODE_ENV || "production";
 
 module.exports = knex(knexConfig[environment]);
